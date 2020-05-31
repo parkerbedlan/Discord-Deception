eb76c023004015d9f6d364e302574e8491ad0cb7
PiyushMewada <piyush.mewada1@gmail.com>
2020-05-31 17:23:53 -0500

    Created Basic Bot Template
    Created the bot file to be able to run the bot

---
+++ bot.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+const Discord = require('discord.js'); 
+const client = new Discord.Client();  
+
+client.on('ready', () => {   
+    console.log(`Logged in as ${client.user.tag}!`); 
+});
+
+client.on('message', async msg => {  
+    if(msg.content.startsWith('?')){
+       switch(msg.content.substring(1)){
+        case "ping":
+            msg.reply("Pong!")
+            break;
+        default:
+            break;
+       }
+}
+
+});
+
+client.login(process.env.BOT_TOKEN);
