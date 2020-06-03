8e2afa9745a03c8681be68643107a8accb858295
Parker Bedlan <parkerbedlan@gmail.com>
2020-06-03 00:44:35 -0500

    added help command

--- bot.js
+++ bot.js
@@ -2,6 +2,19 @@ require("dotenv").config();
 const Discord = require('discord.js'); 
 const client = new Discord.Client();  
 
+const commandList = new Discord.MessageEmbed()
+    .setColor("#8c9eff")
+    .setTitle("Discord Deception Bot Commands:")
+    .setURL("https://github.com/trevorliu13/Discord-Deception/blob/master/bot.js")
+    .setThumbnail("https://i.imgur.com/IchybTu.png")
+    .addFields({
+        name: "**Commands:**",
+        value: "```diff\n" + 
+            "?help: This thing pops up\n" +
+            "?ping: Pong!\n" +
+            "```"
+    });
+
 client.on('ready', () => {   
     console.log(`Logged in as ${client.user.tag}!`); 
 });
@@ -10,9 +23,17 @@ client.on('message', async msg => {
     if(msg.content.startsWith('?')) {
        switch(msg.content.substring(1)) {
         case "ping":
-            msg.reply("Pong!")
+            msg.reply("Pong!");
             break;
+        case "help":
+            msg.channel.send(commandList);
+            break;
+        case "debug":
+            msg.channel.send('this is a test 123');
         default:
+            if (msg.content.substring(1,2) != " ") {
+                msg.reply("That's not a command. To see the full list of commands, type \"?help\".")
+            }
             break;
        }
     }
